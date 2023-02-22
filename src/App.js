9601690d5f346aef41ec98b74edaf35cdf8dfc6a
aldijoko <joko@volantis.io>
2023-02-22 20:43:35 +0700

    add localstorage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Task from './Task';
 import TaskForm from './TaskForm';
 
 const App = () => {
   const [tasks, setTasks] = useState([])
 
+  useEffect(() => {
+    if(tasks.length === 0) return
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    
+  }, [tasks])
+
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem('tasks'))
+    setTasks(tasks)
+  }, [])
+
   const addTask = (name) => {
     setTasks(prev => {
       return [...prev, {name:name, completed: false}]
     })
   }
+
+  const updateComplete = (taskIdx, newComplete) => {
+    setTasks(prev => {
+      const newTasks = [...prev]
+      newTasks[taskIdx].completed = newComplete
+      return newTasks;
+    })
+  }
   return (
     <div className='max-w-xs mx-auto my-5'>
       <TaskForm onAdd={addTask} />
-      {tasks.map(e => (
-        
-        <Task {...e}/>
+      {tasks.map((e, idx) => (
+        <Task {...e} onToggle={completed => updateComplete(idx, completed)}/>
       ))}
       
     </div>
--- src/Task.js
+++ src/Task.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Checbox from './Checbox'
 
-const Task = ({name, complete}) => {
+const Task = ({name, complete, onToggle}) => {
   return (
     <div className='bg-blue-600 rounded-lg px-2 py-1 mt-2 flex items-center'>
-      <Checbox defaultChecked={complete}/>
+      <Checbox 
+        checked={complete}
+        onClick={() => onToggle(!complete)}/>
       {name}
     </div>
   )
