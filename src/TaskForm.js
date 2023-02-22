665fe4ee2cf7e9ea448ae46f6c455d551df886cf
aldijoko <joko@volantis.io>
2023-02-22 20:09:26 +0700

    add checkbox custom and handle submitform

---
+++ src/TaskForm.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+const TaskForm = ({ onAdd }) => {
+    const [taskName, setTaskName] = useState('')
+    const handleSubmit(ev)
+  return (
+    <form 
+      onSubmit={e => onAdd(taskName)}
+      className='border border-blue-600 rounded-md flex p-1 mt-5'>
+        <button className='bg-blue-800 border-none rounded-md cursor-pointer px-2 py-1'>+</button>
+        <input 
+            className='border-none w-full py-1 px-2 block bg-transparent'
+            type="text" 
+            placeholder='New Task' 
+            value={taskName} 
+            onChange={e => setTaskName(e.target.value)} />
+    </form>
+  )
+}
+
+export default TaskForm
